57e816c71489768524cd9fbcea82a99bb89d73ef
Nazar Kushnir <nazar.kushnir2018@gmail.com>
2026-06-27 12:41:21 +0200

    fix CORS policy error

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
